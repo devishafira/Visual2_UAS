8916cc82a0fb0fa80297dd2690d75b6b0f119c8c
devishafira <dvshafira@gmail.com>
2023-07-10 14:42:19 +0800

    mendesain form siswa dan wali kelas

--- Unit2.pas
+++ Unit2.pas
@@ -1,47 +1,37 @@
-unit Unit1;
+unit Unit2;
 
 interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, Grids, DBGrids, StdCtrls, ComCtrls;
+  Dialogs, Grids, DBGrids, StdCtrls;
 
 type
-  TForm1 = class(TForm)
+  TForm2 = class(TForm)
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
-    Label6: TLabel;
-    Label7: TLabel;
     Edit1: TEdit;
     Edit2: TEdit;
     Edit3: TEdit;
-    Edit4: TEdit;
-    Edit5: TEdit;
-    DateTimePicker1: TDateTimePicker;
     ComboBox1: TComboBox;
+    Edit4: TEdit;
+    Label6: TLabel;
+    Label7: TLabel;
     Label8: TLabel;
     Label9: TLabel;
-    Label10: TLabel;
-    Label11: TLabel;
-    Label12: TLabel;
-    Label13: TLabel;
-    Edit6: TEdit;
+    Edit5: TEdit;
     ComboBox2: TComboBox;
+    Edit6: TEdit;
     Edit7: TEdit;
-    Edit8: TEdit;
-    Edit9: TEdit;
-    ComboBox3: TComboBox;
     Button1: TButton;
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
     Button5: TButton;
     DBGrid1: TDBGrid;
-    procedure Label1Click(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -49,13 +39,10 @@ type
   end;
 
 var
-  Form1: TForm1;
+  Form2: TForm2;
 
 implementation
 
 {$R *.dfm}
-
-
-
 
 end.
